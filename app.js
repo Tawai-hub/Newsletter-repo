7eee7d664592f54c50837471695089e103b50255
Tawai-hub <94298076+Tawai-hub@users.noreply.github.com>
2024-02-01 12:47:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post("/failure", (req,res)=> {
     res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log("Listening on port 3000");
 });
 
